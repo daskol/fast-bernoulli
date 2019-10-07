00bb59b39dc8b334f801d27cdef6758b551e284d
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2019-10-07 21:39:38 +0300

    Add quantizer implementation

--- src/executor.cc
+++ src/executor.cc
@@ -10,6 +10,31 @@
 namespace NFastBernoulli {
 
 std::optional<TExecutionPlan> Quantize(double prob, double tol) noexcept {
+    TExecutionPlan plan{.NoSrcBlocks_ = 1};
+    double lhs = 0.0, rhs = 1.0;
+    for (auto it = plan.Ops_.begin(); it + 1 != plan.Ops_.end(); ++it) {
+        if (prob - lhs <= tol && rhs - prob <= tol) {
+            return plan;
+        }
+
+        double mid = lhs + 0.5 * (rhs - lhs);
+
+        if (mid == prob) {
+            return plan;
+        } else if (mid < prob) {
+            *it = EOp::Not;
+            ++it;
+            *it = EOp::And;
+            lhs = mid;
+            plan.NoOps_ += 2;
+            plan.NoSrcBlocks_++;
+        } else if (prob < mid) {
+            *it = EOp::And;
+            rhs = mid;
+            plan.NoOps_ += 1;
+            plan.NoSrcBlocks_++;
+        }
+    }
     return std::nullopt;
 }
 
--- src/executor_test.cc
+++ src/executor_test.cc
@@ -9,9 +9,57 @@
 using NFastBernoulli::EOp;
 using NFastBernoulli::TAvxExecutor;
 using NFastBernoulli::TGeneralExecutor;
+using NFastBernoulli::Quantize;
+
+TEST(Quantize, Zero) {
+    auto plan = Quantize(0.0, 1e-6);
+    ASSERT_TRUE(plan);
+}
+
+TEST(Quantize, One) {
+    auto plan = Quantize(1.0, 1e-6);
+    ASSERT_TRUE(plan);
+}
+
+TEST(Quantize, Half) {
+    auto plan = Quantize(0.5, 1e-6);
+    ASSERT_TRUE(plan);
+    EXPECT_EQ(plan->NoSrcBlocks_, 1);
+    EXPECT_EQ(plan->NoOps_, 0);
+}
+
+TEST(Quantize, Quater) {
+    auto plan = Quantize(0.25, 1e-6);
+    ASSERT_TRUE(plan);
+    EXPECT_EQ(plan->NoSrcBlocks_, 2);
+    EXPECT_EQ(plan->NoOps_, 1);
+    EXPECT_EQ(plan->Ops_[0], EOp::And);
+}
+
+TEST(Quantize, ThreeQuaters) {
+    auto plan = Quantize(0.75, 1e-6);
+    ASSERT_TRUE(plan);
+    EXPECT_EQ(plan->NoSrcBlocks_, 2);
+    EXPECT_EQ(plan->NoOps_, 2);
+    EXPECT_EQ(plan->Ops_[0], EOp::Not);
+    EXPECT_EQ(plan->Ops_[1], EOp::And);
+}
+
+TEST(Quantize, TooPrecise) {
+    auto plan = Quantize(1e-30, 1e-70);
+    ASSERT_FALSE(plan);
+}
 
 TEST(GeneralExecutor, NoOps) {
-    TGeneralExecutor({}).Execute(nullptr, nullptr, 0xffff);
+    std::array<uint64_t, 4> dst;
+    std::array<uint64_t, 4> src{0ul, 1ul, 2ul, 3ul};
+    std::array<uint64_t, 4> res{0ul, 1ul, 2ul, 3ul};
+
+    TGeneralExecutor({.NoSrcBlocks_ = 1}).Execute(src.data(), dst.data(), 1);
+
+    for (size_t i = 0; i != res.size(); ++i) {
+        EXPECT_EQ(dst[i], res[i]);
+    }
 }
 
 TEST(GeneralExecutor, SingleNot) {
@@ -64,7 +112,15 @@ TEST(GeneralExecutor, MultipleOps) {
 }
 
 TEST(AvxExecutor, NoOps) {
-    TAvxExecutor({}).Execute(nullptr, nullptr, 0xffff);
+    alignas(64) std::array<uint64_t, 4> dst;
+    alignas(64) std::array<uint64_t, 4> src{0ul, 1ul, 2ul, 3ul};
+    alignas(64) std::array<uint64_t, 4> res{0ul, 1ul, 2ul, 3ul};
+
+    TAvxExecutor({.NoSrcBlocks_ = 1}).Execute(src.data(), dst.data(), 1);
+
+    for (size_t i = 0; i != res.size(); ++i) {
+        EXPECT_EQ(dst[i], res[i]);
+    }
 }
 
 TEST(AvxExecutor, SingleNot) {
