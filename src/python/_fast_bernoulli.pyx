a739b7c611f5142c100e1bbde11c86a78e64ab32
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2019-10-15 01:02:32 +0300

    Sketch up Python-C++ interfacing code

---
+++ src/python/_fast_bernoulli.pyx
@@ -0,0 +1,42 @@
+#   encoding: utf8
+#   filename: _fast_bernoulli.pyx
+
+from cython.operator cimport dereference as deref
+
+from libcpp cimport bool
+from libcpp.vector cimport vector
+
+
+cdef extern from '<fast-bernoulli/fast-bernoulli.h>' namespace 'NFastBernoulli':
+
+    ctypedef mt19937_64 TRng
+
+    cdef cppclass TAlignedPtr:
+        pass
+
+    cdef cppclass TSamplerPtr:
+        bool operator()(TRng &rng, TAlignedPtr &ptr)
+        TAlignedPtr MakeBuffer(size_t nobits)
+
+    cdef cppclass TSamplerOpts:
+        double Probability_
+        double Tolerance_
+        bool UseJit_
+        bool UseStdSampler_
+
+    TSamplerPtr CreateSampler(const TSamplerOpts &opts)
+    TSamplerPtr CreateSampler(double prob, double tol)
+    vector[bool] Expand(const TAlignedPtr &ptr)
+
+
+def sample(double prob, int nobits, double tol = 1e-3, int seed = 0):
+    cdef TRng *rng = new TRng(seed)
+    cdef TSamplerPtr sampler = CreateSampler(prob, tol)
+    cdef TAlignedPtr ptr = sampler.MakeBuffer(nobits)
+    cdef bool status = sampler(deref(rng), ptr)
+
+    if not status:
+        raise RuntimeError('Sampler is failed to draw instances.')
+
+    cdef vector[bool] result = Expand(ptr)
+    return result
---
+++ src/python/setup.py
@@ -0,0 +1,20 @@
+#   encoding: utf8
+#   filename: setup.py
+
+from setuptools import find_packages, setup
+
+setup(
+    name='fast-bernoulli',
+    version='0.0.0',
+    description='Fast generation of bernoulli-distributed random variables.',
+    url='https://github.com/daskol/fast-bernoulli',
+    author='Daniel Bershatsky',
+    author_email='<daniel.bershatsky@skolkovotech.ru',
+    license='BSD-3-Clause',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD',
+        'Programming Language :: Python :: 3',
+    ],
+)
